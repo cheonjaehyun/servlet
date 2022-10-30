59692df6053a6399c21884e80b98ba034ef11dfc
cheonjaehyun <sjok8701@gmail.com>
2022-10-30 20:40:39 +0900

    feat: 회원 관리 웹 에플리캐이션 요구사항2

--- src/main/java/hello/servlet/domain/member/MemberRepository.java
+++ src/main/java/hello/servlet/domain/member/MemberRepository.java
@@ -1,6 +1,8 @@
 package hello.servlet.domain.member;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class MemberRepository {
@@ -15,5 +17,23 @@ public class MemberRepository {
     }
 
     public MemberRepository() {
+    }
+
+    public Member save(Member member){
+        member.setId(++seq);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    public Member findById(Long id){
+        return store.get(id);
+    }
+
+    public List<Member> findAll(){
+        return new ArrayList<>(store.values());
+    }
+
+    public void  clearStroe(){
+        store.clear();
     }
 }
