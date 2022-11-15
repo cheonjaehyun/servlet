9e1eb25c491b1a9a7f57f5fbfa90bf9419c3d2e4
cheonjaehyun <sjok8701@gmail.com>
2022-11-15 20:35:23 +0900

    feat: View 분리

---
+++ src/main/java/hello/servlet/web/frontcontroller/v2/controller/MemberFormControllerV2.java
@@ -0,0 +1,19 @@
+package hello.servlet.web.frontcontroller.v2.controller;
+
+import hello.servlet.web.frontcontroller.MyView;
+import hello.servlet.web.frontcontroller.v2.ControllerV2;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MemberFormControllerV2 implements ControllerV2 {
+
+    @Override
+    public MyView process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //단축키 컨트롤 + 알트 + n - 인라인 한줄로만들기
+        return new MyView("/WEB-INF/views/new-form.jsp");
+    }
+}
